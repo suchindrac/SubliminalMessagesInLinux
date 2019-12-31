f77db35103625d589ce87da00e601ef2de6dae76
Suchindra Chandrahas <suchi.c@outlook.com>
2019-12-31 06:20:31 +0530

    Made changes to font and randomness

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 . settings
 
-messages=`cat ${message_file}`
 flash_message () {
-	line=`echo $messages| sed 's/I/\r\nI/g' | shuf -n 1`
-	echo $line | osd_cat -p middle -A center &
+	line=`shuf -n 1 ${message_file}`
+	vertical_offset=`echo $RANDOM % 700 + 1 | bc`
+	horizontal_offset=`echo $RANDOM % 800 + 1 | bc`
+	echo $line | osd_cat -c green -o $vertical_offset -i $horizontal_offset -f '-adobe-helvetica-*-*-*--15-*-*-*-*-*-*-*' &
 	sleep ${flash_time}
 	killall -9 osd_cat
 	sleep ${inter_message_time}
